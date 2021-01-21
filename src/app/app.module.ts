c915c75e976057ee1ff0c3182a98d55b117bb6ae
deivy <deivy.spaulonci@qintess.com>
2021-01-21 15:09:38 -0300

    criando tabela de despesa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import {MatIconModule} from '@angular/material/icon'; 
-import {MatButtonModule} from '@angular/material/button'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatGridListModule} from '@angular/material/grid-list'; 
-import {MatInputModule} from '@angular/material/input'; 
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
-import {MatCardModule} from '@angular/material/card'; 
-import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCardModule} from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
-import { FormDespesaComponent } from './page/despesa/form-despesa/form-despesa.component'; 
+import { FormDespesaComponent } from './page/despesa/form-despesa/form-despesa.component';
 
 import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { HomeComponent } from './page/home/home.component';
@@ -54,7 +57,10 @@ const maskConfig: Partial<IConfig> = {
     MatFormFieldModule,
     MatAutocompleteModule,
     MatCardModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
